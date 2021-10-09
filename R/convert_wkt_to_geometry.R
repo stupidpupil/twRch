66fcbfd0b39fe1bd3ffa6bd34fd68202ef526338
Adam Watkins <adam@stupidpupil.co.uk>
2021-10-09 18:16:10 +0100

    Cope slightly better with NAs in WKT columns

--- R/convert_wkt_to_geometry.R
+++ R/convert_wkt_to_geometry.R
@@ -6,7 +6,10 @@ convert_wkt_to_geometry <- function(in_data){
 
   wkt_column = colnames(in_data)[wkt_columns]
 
+  wkt <- in_data %>% pull(wkt_column)
+  wkt <- if_else(is.na(wkt), 'SRID=4326;POINT EMPTY', wkt)
+
   in_data %>%
-    sf::st_set_geometry(sf::st_as_sfc(in_data %>% pull(wkt_column))) %>% 
+    sf::st_set_geometry(sf::st_as_sfc(wkt)) %>% 
     select(-all_of(wkt_column))
 }
