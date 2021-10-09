88d1a034e1e5d182ddfe718e23bb96d607631f11
Adam Watkins <adam@stupidpupil.co.uk>
2021-10-09 11:28:13 +0100

    Add Channel Islands and Isle of Man

--- data-raw/countries.R
+++ data-raw/countries.R
@@ -1,8 +1,8 @@
 tibble(
-  CountryCode = c('W92000004', 'E92000001', 'N92000002', 'S92000003'),
-  CountryName = c('Wales', 'England', 'Northern Ireland', 'Scotland'),
-  CountryNameWelsh = c('Cymru', 'Lloegr', 'Gogledd Iwerddon', 'Yr Alban'),
-  CountryISO_3166_2Code= c('GB-WLS', 'GB-ENG', 'GB-NIR', 'GB-SCT'),
-  CountryAbbr = c('Cym', 'Eng', 'NI', 'Sct') 
+  CountryCode = c('W92000004', 'E92000001', 'N92000002', 'S92000003', 'L93000001', 'M83000003'),
+  CountryName = c('Wales', 'England', 'Northern Ireland', 'Scotland', 'Channel Islands', 'Isle of Man'),
+  CountryNameWelsh = c('Cymru', 'Lloegr', 'Gogledd Iwerddon', 'Yr Alban', 'Ynysoedd y Sianel', 'Ynys_Manaw'),
+  CountryISO_3166_2Code= c('GB-WLS', 'GB-ENG', 'GB-NIR', 'GB-SCT', 'GB-CHA', 'IM'),
+  CountryAbbr = c('Cym', 'Eng', 'NI', 'Sct', 'Cha', 'Man') 
 ) %>%
   write_fst("inst/extdata/CountryCode.fst", compress=100)
