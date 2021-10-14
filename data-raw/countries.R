0aac076732ebfca947b0101db71ff986407cf4df
Adam Watkins <adam@stupidpupil.co.uk>
2021-10-15 00:28:45 +0100

    Drop extra Country fields

--- data-raw/countries.R
+++ data-raw/countries.R
@@ -2,7 +2,8 @@ country_boundaries <- st_read("https://opendata.arcgis.com/datasets/7be6a3c1be3b
   mutate(
     CountryCode = ctry18cd,
     CountryBoundariesGeneralisedClippedWKT = st_as_text(geometry, EWKT=TRUE)) %>% 
-  st_drop_geometry()
+  st_drop_geometry() %>%
+  select(CountryCode, CountryBoundariesGeneralisedClippedWKT)
 
 tibble(
   CountryCode = c('W92000004', 'E92000001', 'N92000002', 'S92000003', 'L93000001', 'M83000003'),
